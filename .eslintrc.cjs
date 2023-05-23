4a8b4a8dcb1b23eda9a68a956dc35466ab316965
Victoria Chen <victoria.c6140@tamu.edu>
2023-05-23 03:18:47 -0500

    eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,16 +19,16 @@ const config = {
     project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: ["next", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
-      "warn",
+      "off"",
       {
         prefer: "type-imports",
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": ["off", { argsIgnorePattern: "^_" }],
   },
 };
 
