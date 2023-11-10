7c19a638c46ab3e02182a809cf8821778bfd7775
Siddarth kotthur <sid@Siddarths-MacBook-Pro.local>
2023-11-10 11:33:32 -0600

    url query

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useSearchParams } from 'next/navigation'
 
 const user = () => {
+  const searchParams = useSearchParams()
+
+  const event = searchParams?.get('event')
+
+
   return (
+    <>
+    <h1>{event}</h1>
     <div>Please enter your UIN</div>
+    </>
   )
 }
 
