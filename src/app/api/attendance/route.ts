9cc18dc69d55349968852fab57c1e7ac91fdcf2c
Siddarth kotthur <sid@Siddarths-MBP.lan>
2023-12-22 22:11:23 -0600

    sase attendance

--- src/app/api/attendance/route.ts
+++ src/app/api/attendance/route.ts
@@ -1,12 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client';
+import { NextRequest } from 'next/server';
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
-    switch(req.method) {
-        case "POST": 
-            res.status(200).json({ message: "POST method called" });
-            break;
-        case "GET":
-            res.status(200).json({ message: "GET method called" });
-            break;
+const client = new PrismaClient();
+
+export const POST = async (request: NextRequest) => {
+    const {uin, event} = await request.json()
+
+    const user = await client.users.findUnique({
+        where: {
+            UIN: uin
+        }
+    })
+
+    if (user){
+        const response = await client.users.update({
+            where:{
+                UIN: uin,
+            },
+            data: {
+                [event]: 1,
+            }
+        })
+    }else{
+        const response = await client.users.create({
+            data: {
+                UIN: uin,
+                [event]: 1,
+            }
+        })
     }
+
+    return new Response(JSON.stringify({
+        message: "User creation failed"
+    }), {
+        status: 500
+    })
+
 }
--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
+import { useEventFetch } from '~/hooks/useEventFetch'
 
 const user = () => {
+  const current_event = "GBM1"
+
   const searchParams = useSearchParams()
+  const [uin, setUin] = useState('')
 
   const event = searchParams?.get('event')
 
+  const check_uin = (uin: string) => {
+    const uin_regex = new RegExp("^[0-9]{9}$")
+
+    return uin_regex.test(uin)
+  }
+
+  const submitAttendance = async () => {
+    if (event === current_event){
+      if (!check_uin(uin)){
+        alert("Invalid UIN")
+        return
+      }
+      
+      const response = await fetch("/api/attendance", {
+        cache: 'no-cache',
+        method: 'POST',
+        headers: {
+              'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ uin: uin, event: event }),
+      });
+      
+      alert("Attendance Marked!")
+    }else{
+      alert("Event not active")
+    }
+  }
+
 
   return (
-    <>
-    <h1>{event}</h1>
-    <div>Please enter your UIN</div>
-    </>
+  <div className='absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2'>
+    <input type="text" placeholder='your UIN' value={uin} onChange={e => { setUin(e.currentTarget.value); }} className='m-3 bg-transparen text-white font-semibol py-2 px-4 border border-blue-500 rounded'/>
+    <button onClick={submitAttendance} className='m-3 bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded'>Mark Attendance</button>
+  </div>
   )
 }
 
